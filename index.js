fea750c722d25636443d3fc755ec53c37e1c0d8d
EvanCharlesHolloway <128486114+EvanCharlesHolloway@users.noreply.github.com>
2023-12-01 15:19:26 +0000

    Final Change

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 // Middleware to parse incoming requests with JSON payloads
 app.use(express.json());
 
 // Routes
-const gameRoutes = require('./routes/games');
+const gameRoutes = require('./QAP3 Combined/routes/games');
 app.use('/api/games', gameRoutes);
+
+// Define a route for the root path ("/")
+app.get('/', (req, res) => {
+  res.send('Welcome to HollowGames! Explore our collection of exciting games.');
+});
 
 // Start the server
 app.listen(port, () => {
